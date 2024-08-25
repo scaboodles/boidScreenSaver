5b580c5083a98ff73833e1b7305bcea548320281
scaboodles <owenj.wolff2@gmail.com>
2024-08-25 15:20:45 -0400

    Initial Commit

---
+++ BoidScreenSaver/BoidScreenSaverView.h
@@ -0,0 +1,12 @@
+//
+//  BoidScreenSaverView.h
+//  BoidScreenSaver
+//
+//  Created by Owen Wolff on 8/25/24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BoidScreenSaverView : ScreenSaverView
+
+@end
